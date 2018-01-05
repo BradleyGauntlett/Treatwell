d9ed00bb0678597ae1a2a8ce36260dbfc7d659c4
BradleyGauntlett <Bradley.Gauntlett.2013@live.rhul.ac.uk>
2018-01-05 11:08:19 +0000

    Added condition check

--- Treatwell.java
+++ Treatwell.java
@@ -6,49 +6,56 @@
  */
 public class Treatwell {
 	public static void main(String [] args){
-		drawBox(10, 40);
+		drawBox(4, 7);
 	}
 	
 	/**
-	 * drawBow() method - takes height and width as args and based on these values will draw the appropriate box
-	 * @param h
-	 * @param w
+	 * drawBow(int, int) - takes height and width as args and based on these values will draw the appropriate box
+	 * @param h - represents box height
+	 * @param w - represents box width
 	 */
 	public static void drawBox(int h, int w){
 		char[][] box = new char[h][w];
-		/*Populate box*/
-		for(int i= 0; i<h;i++){
-			for(int j=0; j<w;j++){
-				
-				if( (i == 0 || i == h-1) && (j != 0 || j != w-1)){
-					box[i][j] = '─';
+		
+		if (h ==0 || w ==0){
+			System.out.println("Error: Enter values greater than 0");
+		} else {
+			/*Populate box*/
+			for(int i= 0; i<h;i++){
+				for(int j=0; j<w;j++){
+					
+					if( (i == 0 || i == h-1) && (j != 0 || j != w-1)){
+						box[i][j] = '─';
+					}
+					if(j == 0 || j == w-1){
+						box[i][j] = '|';
+					}
+					if(i==0 && j ==0){
+						box[i][j] = '┌';
+					}
+					if(j==w-1 && i==0){
+						box[i][j] = '┐';
+					}
+					
+					if(j==0 && i==h-1){
+						box[i][j] = '└';
+					}
+					
+					if(j==w-1 && i==h-1){
+						box[i][j] = '┘';
+					}	
 				}
-				if(j == 0 || j == w-1){
-					box[i][j] = '|';
-				}
-				if(i==0 && j ==0){
-					box[i][j] = '┌';
-				}
-				if(j==w-1 && i==0){
-					box[i][j] = '┐';
-				}
-				
-				if(j==0 && i==h-1){
-					box[i][j] = '└';
-				}
-				
-				if(j==w-1 && i==h-1){
-					box[i][j] = '┘';
-				}	
 			}
+			
+			/*Print box*/
+			for(int i= 0; i<h;i++){
+				for(int j=0; j<w;j++){
+					System.out.print(box[i][j]);
+				}
+				System.out.println();
+			}
+			
 		}
 		
-		/*Print box*/
-		for(int i= 0; i<h;i++){
-			for(int j=0; j<w;j++){
-				System.out.print(box[i][j]);
-			}
-			System.out.println();
-		}
 	}
 }
